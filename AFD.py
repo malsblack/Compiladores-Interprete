3e9ec937eee5099b7ed0267834aa138b2aab621b
malsblack <sebastian.martinez.lopez98@gmail.com>
2023-06-13 23:10:38 -0600

    Primera etapa completa

--- AFD.py
+++ AFD.py
@@ -1,43 +1,47 @@
 from colorama import init, Fore, Style
 import json
+from AnalizadorSintantico import analizador_sintantico
+
 tokens=json.load(open("tokens.json"))
 palabras_reservadas=tokens["Palabas_reservadas"]
 numeros=tokens["Numeros"]
 operadores=tokens["Operadores"]
 operadores_especiales=tokens["Operadores_especiales"]
-palabras=[]
-numeros_lista=[]  
-operadores_lista=[]
 novalidos=[]
 col_width=20
 data=[]
+data2=[]
+
+
 def seperacion(lista):
     nueva_lista=[]
     for elemento in lista:
-        if elemento[0] in operadores_especiales:
-            nueva_lista.append(elemento)
+        if elemento[0][0] not in operadores:
+            pass
         else:
-            for i in elemento[0]:
-                nueva_lista.append([i,elemento[1]])
-
-    return(nueva_lista)
-    
+            if elemento[0][0:2] in operadores:
+                pass
+            else:
+                if elemento[0][0:2] not in operadores_especiales:
+                    for i in elemento[0][0:2]:
+                        lista.insert(lista.index(elemento),[i,elemento[1]])
+                    lista.pop(lista.index(elemento))
+                else:
+                    pass
 def escritura(lista):
     for elemento in lista:
-        
         if elemento[0] in palabras_reservadas:
-            data.append([palabras_reservadas.get(elemento[0]),elemento[0],elemento[0],elemento[1]])
+            data2.append([palabras_reservadas.get(elemento[0]),elemento[0],elemento[0],elemento[1]])
         elif elemento[0][0]=='"':
-            data.append(["CADENA",elemento[0],elemento[0][1:-1],elemento[1]])
+            data2.append(["CADENA",elemento[0],elemento[0][1:-1],elemento[1]])
         elif elemento[0] in operadores:
-            data.append([operadores.get(elemento[0]),elemento[0],"",elemento[1]])
+            data2.append([operadores.get(elemento[0]),elemento[0],"",elemento[1]])
         elif elemento[0] in operadores_especiales:
-            data.append([operadores_especiales.get(elemento[0]),elemento[0],"",elemento[1]])
+            data2.append([operadores_especiales.get(elemento[0]),elemento[0],"",elemento[1]])
         elif elemento[0]=="NUMERO":
-            data.append([elemento[0],elemento[1],elemento[2],elemento[3]])
+            data2.append([elemento[0],elemento[1],elemento[2],elemento[3]])
         else:
-            data.append(["IDENTIFICADOR",elemento[0],elemento[0],elemento[1]])
-            
+            data2.append(["IDENTIFICADOR",elemento[0],elemento[0],elemento[1]])
 def lectura(archivo):
     aux=False
     for reglon,linea in enumerate(archivo):
@@ -58,7 +62,7 @@ def lectura(archivo):
                     else:
                         palabra+=caracter
                         if constructor_operador!="":
-                            operadores_lista.append([constructor_operador,reglon+1])
+                            data.append([constructor_operador,reglon+1])
                             constructor_operador=""
 
                     
@@ -76,33 +80,30 @@ def lectura(archivo):
                             constructor_numero+=caracter
                             
                         if constructor_operador!="":
-                            operadores_lista.append([constructor_operador,reglon+1])
+                            data.append([constructor_operador,reglon+1])
                             constructor_operador=""
 
             elif caracter in operadores: #validacion operadores
-                if comentario!="":
-                    comentario+=caracter
-                else:
                     
-                    if cadena!="":
-                        cadena+=caracter
+                if cadena!="":
+                    cadena+=caracter
+                else:
+                    if palabra!="":
+                        data.append([palabra,reglon+1])
+                        palabra=""
+                    if constructor_numero!="":
+                        data.append(["NUMERO",constructor_numero,constructor_numero,reglon+1])
+                        constructor_numero=""
+                    if constructor_operador!="":
+                        constructor_operador+=caracter
                     else:
-                        if palabra!="":
-                            palabras.append([palabra,reglon+1])
-                            palabra=""
-                        if constructor_numero!="":
-                            numeros_lista.append(["NUMERO",constructor_numero,constructor_numero,reglon+1])
-                            constructor_numero=""
-                        if constructor_operador!="":
-                            constructor_operador+=caracter
-                        else:
-                            constructor_operador+=caracter
+                        constructor_operador+=caracter
                     if constructor_operador=="/*":
                         comentario+=constructor_operador
                         constructor_operador=""
                     elif constructor_operador=="*/":
                         comentario+=constructor_operador
-                        data.append(["COMENTARIO",comentario,comentario,reglon+1])
+                        data.append(["COMENTARIO",comentario,comentario[2:-2],reglon+1])
                         constructor_operador=""
                     
                     
@@ -115,17 +116,17 @@ def lectura(archivo):
                         palabras.append([palabra,reglon+1])
                         palabra=""
                     if constructor_numero!="":
-                        numeros_lista.append(["NUMERO",constructor_numero,constructor_numero,reglon+1])
+                        data.append(["NUMERO",constructor_numero,constructor_numero,reglon+1])
                         constructor_numero=""
                     if constructor_operador!="":
-                        operadores_lista.append([constructor_operador,reglon+1])
+                        data.append([constructor_operador,reglon+1])
                         constructor_operador=""
                     if cadena=="":
                         cadena+=caracter
                     else:
                         if cadena.count('"')==1:
                             cadena+=caracter
-                            palabras.append([cadena,reglon+1])
+                            data.append([cadena,reglon+1])
                             cadena=""
                         else:
                             pass
@@ -133,10 +134,10 @@ def lectura(archivo):
                     
             elif caracter ==" ":
                 if palabra!="":
-                    palabras.append([palabra,reglon+1])
+                    data.append([palabra,reglon+1])
                     palabra=""
                 if constructor_numero!="":
-                    numeros_lista.append(["NUMERO",constructor_numero,constructor_numero,reglon+1])
+                    data.append(["NUMERO",constructor_numero,constructor_numero,reglon+1])
                     constructor_numero=""
                 if cadena!="":
                     cadena+=caracter
@@ -149,11 +150,11 @@ def lectura(archivo):
                     pass
                 else:
                     if palabra!="":
-                        palabras.append([palabra,reglon+1])
+                        data.append([palabra,reglon+1])
                     elif constructor_numero!="":
-                        numeros_lista.append(["NUMERO",constructor_numero,constructor_numero,reglon+1])
+                        data.append(["NUMERO",constructor_numero,constructor_numero,reglon+1])
                     elif constructor_operador!="":
-                        operadores_lista.append([constructor_operador,reglon+1])
+                        data.append([constructor_operador,reglon+1])
                     else:
                         pass
 
@@ -166,16 +167,15 @@ print((Fore.BLUE+"ESCUELA SUPERIOR DE COMPUTO").center(50," "))
 print((Fore.RED+"COMPILADORES").center(50," "))
 print((Fore.GREEN+ Style.BRIGHT+ "   SELU V1.1   ").center(50," "))
 
-data=[["IDENTIFICADOR","LEXEMA","LITERAL","LINEA"]]
-print(Fore.BLUE + "{:{width}} {:{width}} {:{width}} {:{width}}".format(data[0][0], data[0][1],data[0][2],data[0][3], width=col_width) + Style.RESET_ALL)
+data2=[["IDENTIFICADOR","LEXEMA","LITERAL","LINEA"]]
+print(Fore.BLUE + "{:{width}} {:{width}} {:{width}} {:{width}}".format(data2[0][0], data2[0][1],data2[0][2],data2[0][3], width=col_width) + Style.RESET_ALL)
 print("-" *  4)
 
 lectura(open("codigo.txt"))
-escritura(palabras)
-escritura(numeros_lista)
-escritura(seperacion(operadores_lista))
-
+seperacion(data)
+escritura(data)
+analizador_sintantico(data2)
     
-    
-for row in data[1:]:
-    print(Fore.GREEN+"{:{width}}".format(row[0],width=col_width),Fore.WHITE+ "{:{width}} {:{width}} {:{width}}".format(row[1],row[2], row[3], width=col_width-1))
+#for row in data2[1:]:
+    #print(row)
+    #print(Fore.GREEN+"{:{width}}".format(row[0],width=col_width),Fore.WHITE+ "{:{width}} {:{width}} {:{width}}".format(row[1],row[2], row[3], width=col_width-1))
---
+++ AnalizadorSintantico.py
@@ -0,0 +1,4 @@
+def analizador_sintantico(lista):
+    VAR_DECL=[]
+    for DECLARATION in lista[1:]:
+        print(DECLARATION)
